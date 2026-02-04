a8a76f3c9a900a051ccf9db4273d175ab04fa19a
GZod01 <gzod01@gzod01.fr>
2026-02-04 10:50:28 +0100

    add stats automation

---
+++ include/Common/movementAction.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <Common/robotConfig.hpp>
+#include <Common/communication_simplified.hpp>
+class MovementAction{
+    RobotConfig config;
+    CommPrincipale* communication;
+};
