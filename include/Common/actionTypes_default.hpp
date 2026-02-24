0123f92c18242bfa8f8ec9ac70482968edc6b93d
GZod01 <gzod01@gzod01.fr>
2026-02-24 12:46:59 +0100

    lo

--- include/Common/actionTypes_default.hpp
+++ include/Common/actionTypes_default.hpp
@@ -1,9 +1,10 @@
 #pragma once
 #include <Common/actionType.hpp>
-void _forwardFunc(ActionHandler*parent,DataArgumentType arg){
+// void _forwardFunc(ActionHandler*parent,DataArgumentType arg){
     
-}
+// }
 /// @brief predefined default actionTypes list
-extern std::map<std::string,ActionType> actionTypes = (std::map<std::string,ActionType>){
-    (std::pair<std::string,ActionType>){"FORWARD",ActionType(0,)}
-};
+extern std::map<std::string,ActionType> actionTypes;
+//  = (std::map<std::string,ActionType>){
+//     (std::pair<std::string,ActionType>){"FORWARD",ActionType(0,_forwardFunc)}
+// };
